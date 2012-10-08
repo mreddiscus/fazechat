ce898309058d8835a90791b963a22cf554a5579d
zero5100 <akellerstrass@gmail.com>
2012-10-08 23:42:30 +0000

    Add blank chatPanel widget

---
+++ public/widgets/chatPanel/chatPanel.client.js
@@ -0,0 +1,15 @@
+feather.ns("fazechat");
+(function() {
+  fazechat.chatPanel = feather.Widget.create({
+    name: "fazechat.chatPanel",
+    path: "widgets/chatPanel/",
+    prototype: {
+      onInit: function() {
+        
+      },
+      onReady: function() {
+        
+      }
+    }
+  });
+})();
