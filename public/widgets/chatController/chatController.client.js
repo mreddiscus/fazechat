e30f8c931ca8bbd72b79caac914226f4e11fa70e
zero5100 <akellerstrass@gmail.com>
2012-10-12 02:52:32 +0000

    Create controller widgets

---
+++ public/widgets/chatController/chatController.client.js
@@ -0,0 +1,15 @@
+feather.ns("fazechat");
+(function() {
+  fazechat.chatController = feather.Widget.create({
+    name: "fazechat.chatController",
+    path: "widgets/chatController/",
+    prototype: {
+      onInit: function() {
+        
+      },
+      onReady: function() {
+        
+      }
+    }
+  });
+})();
---
+++ public/widgets/mediaController/mediaController.client.js
@@ -0,0 +1,15 @@
+feather.ns("fazechat");
+(function() {
+  fazechat.mediaController = feather.Widget.create({
+    name: "fazechat.mediaController",
+    path: "widgets/mediaController/",
+    prototype: {
+      onInit: function() {
+        
+      },
+      onReady: function() {
+        
+      }
+    }
+  });
+})();
