442cc7ab24eaae139dc20d80e1eec5d7a798033f
zero5100 <akellerstrass@gmail.com>
2012-10-19 03:08:45 +0000

    Move the content from app to the respective widget controllers

--- public/widgets/app/app.client.js
+++ public/widgets/app/app.client.js
@@ -7,45 +7,17 @@ feather.ns("fazechat");
       onInit: function() {
         
       },
+      
       onReady: function() {
         var me = this;
         
         me.bindUI();
       },
+      
       bindUI: function() {
         var me = this;
       
-        me.bindTabs();
-      },
-      bindTabs: function() {
-        var me = this;
         
-        me.get('#room-nav a').click(function(e) {
-          e.preventDefault();
-          
-          $(this).tab('show');
-        });
-        
-        me.get('a[data-toggle="tab"]').on('shown', function(e) {
-          var stripHrefId = function(target) {
-            return target.href.split('#')[1];
-          };
-          
-          // e.target = activated tab
-          // e.relatedTarget = previous tab
-          
-          me.get('#' + stripHrefId(e.relatedTarget)).removeClass('active'); // hide the old tab
-          me.get('#' + stripHrefId(e.target)).addClass('active'); // show the new tab
-          
-        });     
-        
-      },
-      addTab: function() {
-        // <widget id="$[tabData.id]" path="widgets/chatWidget/">
-          // <options>
-            // <option name="roomId" value="$[tabData.id]" />
-          // </options>
-        // </widget>
       }
     }
   });
