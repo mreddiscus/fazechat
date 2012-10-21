ee308762a140130cd93b74957a9d08265a6536de
zero5100 <akellerstrass@gmail.com>
2012-10-21 05:03:01 +0000

    Load chatpanel widget into controller

--- public/widgets/chatController/chatController.client.js
+++ public/widgets/chatController/chatController.client.js
@@ -15,7 +15,9 @@ feather.ns("fazechat");
       
       bindUI: function() {
         var me = this;
-      
+        
+        me.addTab({id: "12345", name: "test"});
+        me.addTab({id: "54321", name: "test2"});
         me.bindTabs();
       },
       
@@ -35,20 +37,35 @@ feather.ns("fazechat");
           
           // e.target = activated tab
           // e.relatedTarget = previous tab
-          
-          me.get('#' + stripHrefId(e.relatedTarget)).removeClass('active'); // hide the old tab
+          if (e.relatedTarget) { // If there is a previous tab
+            me.get('#' + stripHrefId(e.relatedTarget)).removeClass('active'); // hide the old tab
+          }
           me.get('#' + stripHrefId(e.target)).addClass('active'); // show the new tab
           
         });     
         
       },
       
-      addTab: function() {
-        // <widget id="$[tabData.id]" path="widgets/chatWidget/">
-          // <options>
-            // <option name="roomId" value="$[tabData.id]" />
-          // </options>
-        // </widget>
+      addTab: function(tabData) {
+        var me = this;
+        var tabId = me.id + "_" + tabData.id;
+        
+        me.get("#chat-tabs-nav").append('<li><a data-toggle="tab" href="#' + tabId + '">' + tabData.name + '</a></li>');
+        var $new = $(this).closest('li').clone().appendTo('#cart ul')
+        me.get("#chat-tabs-body").append('<div class="tab-pane" id="' + tabId + '" ></div>').each(function() {
+
+          feather.Widget.load({
+            path: "widgets/chatController/chatPanel/",
+            serverOptions: {
+            },
+            clientOptions: {
+              parent: me,
+              container: $("#" + tabId),
+              keepContainerOnDispose: true
+            }
+          });
+        });
+
       }
     }
   });
--- public/widgets/socialController/socialController.client.js
+++ public/widgets/socialController/socialController.client.js
@@ -8,6 +8,38 @@ feather.ns("fazechat");
         
       },
       onReady: function() {
+        var me = this;
+        
+        me.bindUI();
+      },
+      
+      bindUI: function() {
+        var me = this;
+        
+        me.bindTabs();
+      },
+      
+      bindTabs: function() {
+        var me = this;
+        
+        me.get('#social-tabs-nav a').click(function(e) {
+          e.preventDefault();
+          
+          $(this).tab('show');
+        });
+        
+        me.get('a[data-toggle="tab"]').on('shown', function(e) {
+          var stripHrefId = function(target) {
+            return target.href.split('#')[1];
+          };
+          
+          // e.target = activated tab
+          // e.relatedTarget = previous tab
+          
+          me.get('#' + stripHrefId(e.relatedTarget)).removeClass('active'); // hide the old tab
+          me.get('#' + stripHrefId(e.target)).addClass('active'); // show the new tab
+          
+        });     
         
       }
     }
