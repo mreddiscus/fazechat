b2afe9ecbf6aab8833db51ee7244791623f37878
shawn <shawn@vmUbuntuShawn.campus.ittesi.com>
2012-10-21 19:28:02 -0500

    added donate button and testing widget

---
+++ public/widgets/shawn1/shawn1.client.js
@@ -0,0 +1,15 @@
+feather.ns("fazechat");
+(function() {
+  fazechat.shawn1 = feather.Widget.create({
+    name: "fazechat.shawn1",
+    path: "widgets/shawn1/",
+    prototype: {
+      onInit: function() {
+        
+      },
+      onReady: function() {
+        
+      }
+    }
+  });
+})();
---
+++ public/widgets/shawn1/shawn1.server.js
@@ -0,0 +1,6 @@
+exports.getWidget = function(feather, cb) {
+  cb(null, {
+    name: "fazechat.shawn1",
+    path: "widgets/shawn1/"
+  });
+};
