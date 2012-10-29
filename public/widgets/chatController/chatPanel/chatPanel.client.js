94267d5534afe3ede691e94ec0bd38c97729f7ed
zero5100 <akellerstrass@gmail.com>
2012-10-29 02:33:32 +0000

    Chat form submit

--- public/widgets/chatController/chatPanel/chatPanel.client.js
+++ public/widgets/chatController/chatPanel/chatPanel.client.js
@@ -8,7 +8,22 @@ feather.ns("fazechat");
         
       },
       onReady: function() {
+        var me = this;
         
+        // Called when you send a message
+        me.domEvents.bind(me.get("#chat"), "submit", function(e) {
+          e.preventDefault();
+          
+          debugger;
+        });
+        
+        me.get("#chat-input").keypress(function(e) {      
+
+          if (e.which === 13 && !(e.altKey || e.ctrlKey || e.shiftKey)) { // If the pressed key was enter
+            e.preventDefault();
+            me.get("#chat").submit();
+          }
+        });
       }
     }
   });
